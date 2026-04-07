75ef2f2a7f1a0dc57b215df23c90c1e29340162b
Saket shaurya <69414031+Saket2610@users.noreply.github.com>
2026-04-07 12:26:29 +0530

    Create structures.cpp

---
+++ structures.cpp
@@ -0,0 +1,35 @@
+
+void solve(){
+
+    int n;
+    cin>>n;
+    vector vec(2 , vector<int>(n));
+    vector<vector<int>> vec1(2 , vector<int>(n));
+   
+
+    vector<int> a[n];
+    // n - vectors 
+
+    vector<int> a(n);
+    // 1 - vector of size n ; 
+
+    vector<int> a(n,0);
+    // 1 -vector of size n all initally 0 ;
+
+
+    const int n = 5;
+    vector<array<int, n>> vec2(2, array<int, n>{});
+    // only works with constant(means no input)
+
+
+    const int n = 5;
+    vector<array<int,n>> vec2(2);
+    // it means 2 rows and n columns same as above 
+    // cant take input too 
+
+
+    int m; 
+    cin>>m;
+    int a[m][m];
+    // can take input 
+}

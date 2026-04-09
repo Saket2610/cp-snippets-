7ddfffa92d079b496960e140de93cf4bf1b9463b
Saket shaurya <69414031+Saket2610@users.noreply.github.com>
2026-04-09 23:30:36 +0530

    Update vector_tricks.cpp

--- vector_tricks.cpp
+++ vector_tricks.cpp
@@ -4,4 +4,15 @@ void solve(){
 
     rev.insert(rev.end(), x.begin(), x.end());
     // adds vector x at the end of vector rev 
+
+    iota(p[j].begin(), p[j].end(), 0);
+    // fill the vector p with 0 
+
+    
+    std::sort(p[i].begin(), p[i].end(),
+            [&](int x, int y) {
+                return a[x][i] < a[y][i];
+            });
+    // sorted values of a transfers to p 
+    // a remains unchanged 
 } 

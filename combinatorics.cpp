74f602cdfa2752a4e12dd6020a8aec0522c3c876
Saket shaurya <69414031+Saket2610@users.noreply.github.com>
2026-04-13 06:41:48 +0530

    Update combinatorics.cpp

--- combinatorics.cpp
+++ combinatorics.cpp
@@ -1,1 +1,67 @@
+ 
+const int facN = 1e6 + 5;
+const int mod = 1e9 + 7; // 998244353
+int ff[facN], iff[facN];
+bool facinit = false;
 
+int add(int x, int y)
+{
+	return (x + y) % mod;
+}
+
+int sub(int x, int y)
+{
+	return add(x, mod - y);
+}
+
+int mul(int x, int y)
+{
+	return (x * 1ll * y) % mod;
+}
+
+int power(int x, int y){
+	if (y == 0) return 1;
+ 
+	int v = power(x, y / 2);
+	v = 1LL * v * v % mod;
+ 
+	if (y & 1) return 1LL * v * x % mod;
+	else return v;
+}
+ 
+void factorialinit(){
+	facinit = true;
+	ff[0] = iff[0] = 1;
+ 
+	for (int i = 1; i < facN; i++){
+		ff[i] = 1LL * ff[i - 1] * i % mod;
+	}
+ 
+	iff[facN - 1] = power(ff[facN - 1], mod - 2);
+	for (int i = facN - 2; i >= 1; i--){
+		iff[i] = 1LL * iff[i + 1] * (i + 1) % mod;
+	}
+}
+ 
+int C(int n, int r){
+	if (!facinit) factorialinit();
+ 
+	if (n == r) return 1;
+ 
+	if (r < 0 || r > n) return 0;
+	return 1LL * ff[n] * iff[r] % mod * iff[n - r] % mod;
+}
+ 
+int P(int n, int r){
+	if (!facinit) factorialinit();
+ 
+	assert(0 <= r && r <= n);
+	return 1LL * ff[n] * iff[n - r] % mod;
+}
+ 
+int Solutions(int n, int r){
+	//solutions to x1 + ... + xn = r 
+	//xi >= 0
+ 
+	return C(n + r - 1, n - 1);
+}

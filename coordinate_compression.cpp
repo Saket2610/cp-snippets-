5a2ed132226fcf9ad1c3fe7c1c69bbac31644439
Saket shaurya <69414031+Saket2610@users.noreply.github.com>
2026-04-17 00:29:18 +0530

    Update coordinate_compression.cpp

--- coordinate_compression.cpp
+++ coordinate_compression.cpp
@@ -1,13 +1,10 @@
-std::vector<int> v(n), p(n);
+std::vector<int> v(n);
 for (int i = 0; i < n; i++) {
-    std::cin >> v[i] >> p[i];
+    std::cin >> v[i] ;
 }
-auto vs = v, ps = p;
+auto vs = v;
 std::sort(vs.begin(), vs.end());
-    std::sort(ps.begin(), ps.end());
     
 for (int i = 0; i < n; i++) {
     v[i] = std::lower_bound(vs.begin(), vs.end(), v[i]) - vs.begin();
-    p[i] = std::lower_bound(ps.begin(), ps.end(), p[i]) - ps.begin();
 }
-    

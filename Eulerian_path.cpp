4292feb01e7e34772ddd76c41ba41fea570741b3
Saket shaurya <69414031+Saket2610@users.noreply.github.com>
2026-04-17 04:10:20 +0530

    Update Eulerian_path.cpp

--- Eulerian_path.cpp
+++ Eulerian_path.cpp
@@ -12,6 +12,7 @@ auto dfs = [&](this auto &&self, int x) -> void {
     }
 };
 dfs(s);
+// c++23 
 // cur[i] = prevents re-scanning edges → O(n) total
 // must have exactly two vertex with odd degrees(more than 2 --> euler path not possible)
 // we always start at one of the odd degee vertex and it ends on the other one .

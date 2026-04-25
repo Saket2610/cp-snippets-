a206170b413238d81d7bb94b9724900c960fc270
Saket shaurya <69414031+Saket2610@users.noreply.github.com>
2026-04-25 20:55:17 +0530

    Update bitmask_tricks.cpp

--- bitmask_tricks.cpp
+++ bitmask_tricks.cpp
@@ -1,9 +1,13 @@
 int la = a & -a;
 // lowest set bit (1010 --> 2 (10)
-
 -a = ~a + 1 --> fact 
 
 a / la = odd part of a 
 
 
-
+// Declares 100005 bits, all initialized to 0
+bitset<100005> f{};
+f[500] = 1;
+f.set(100000); 
+cout << f[500] << std::endl; // Output: 1
+cout << f.size() << std::endl; // Output: 100005

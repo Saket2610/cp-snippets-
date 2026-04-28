bcf546da97bbb4bd551b4de4d9947e679494b250
Saket shaurya <69414031+Saket2610@users.noreply.github.com>
2026-04-28 22:50:13 +0530

    Update queue_deque.cpp

--- queue_deque.cpp
+++ queue_deque.cpp
@@ -1,1 +1,17 @@
+void solve(){ 
+    deque<int> q;
+    int x = q.back();
+    q.pop_back();
+    q.push_back(i)
+    q.pop_front();
+    q.push_front(i);
 
+  //////
+    queue<int> q;
+    q.push(10);
+    cout<<q.back();
+    cout<<q.front();
+    q.pop();
+    q.size();
+
+}

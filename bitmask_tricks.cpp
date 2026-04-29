e785e483110d9181700aad2b14ff988732d9045d
Saket shaurya <69414031+Saket2610@users.noreply.github.com>
2026-04-30 00:46:51 +0530

    Update bitmask_tricks.cpp

--- bitmask_tricks.cpp
+++ bitmask_tricks.cpp
@@ -11,3 +11,13 @@ f[500] = 1;
 f.set(100000); 
 cout << f[500] << std::endl; // Output: 1
 cout << f.size() << std::endl; // Output: 100005
+
+
+//
+int binaryToInt(string binary) {
+    return stoi(binary, nullptr, 2);
+}
+cout << binaryToInt("0010");   // 2
+// stoi  31 bits
+// stol  31/63 bits
+// stoll 63 bits // safest 

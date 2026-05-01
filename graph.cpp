066b5e7974ed14063a148bc83e974f17d60eb53b
Saket shaurya <69414031+Saket2610@users.noreply.github.com>
2026-05-01 07:38:02 +0530

    Create graph.cpp

---
+++ graph.cpp
@@ -0,0 +1,9 @@
+void solve(){
+    vector<int> u(m), v(m), w(m);
+    for (int i = 0; i < m; i++) {
+        std::cin >> u[i] >> v[i] >> w[i];
+        u[i]--, v[i]--;
+    }
+  // How to input a graph with weights
+
+}

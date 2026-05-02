fc2ad2b996f7f09f4eec45117cbbbd65ff2f2834
Saket shaurya <69414031+Saket2610@users.noreply.github.com>
2026-05-02 18:07:08 +0530

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -5,5 +5,14 @@ void solve(){
         u[i]--, v[i]--;
     }
   // How to input a graph with weights
+  vector<vector<pair<int, int>>> E(n);
+  for (int i = 0; i < m; i++){
+    int u, v, w;
+    cin >> u >> v >> w;
+    u--;
+    v--;
+    E[u].push_back(make_pair(w, v));
+    E[v].push_back(make_pair(w, u));
+  }
 
 }

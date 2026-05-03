298a66d898695ddbb4325768a3777ecf122993a3
Saket shaurya <69414031+Saket2610@users.noreply.github.com>
2026-05-03 06:24:26 +0530

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -15,4 +15,16 @@ void solve(){
     E[v].push_back(make_pair(w, u));
   }
 
-}
+
+    ////
+    // iterating the matrix is all the 4 directions 
+    // for matrix int a = i * k + j ; 
+    // for converting the 2-d matrix into 1d
+    vector<std::pair<int, int>> moves{{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+     for (auto [dx, dy] : moves) {
+                int nx = x + dx;
+                int ny = y + dy;
+                 if(a[nx][ny] > x ){
+                 }
+             }   
+     }

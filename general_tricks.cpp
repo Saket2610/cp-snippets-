5671ecf98f69b48c1744dbf48f9f39690f6e289c
Saket shaurya <69414031+Saket2610@users.noreply.github.com>
2026-05-13 08:28:24 +0530

    Update general_tricks.cpp

--- general_tricks.cpp
+++ general_tricks.cpp
@@ -12,5 +12,9 @@ void solve{
     int a = 12, b = 18;
     std::cout << "LCM: " << std::lcm(a, b); // Output: 36
 
+    int a = max_element(nums.begin(), nums.end()) - nums.begin();
+    // returns the index of the max element
 
+    int a = min_element(nums.begin(), nums.end()) - nums.begin();
+    // returns the index of the min element 
 }

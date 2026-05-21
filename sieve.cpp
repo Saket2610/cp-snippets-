3d4f7d2d0729e2b4f680c7b20d253e7b04c86f15
Saket shaurya <69414031+Saket2610@users.noreply.github.com>
2026-05-21 18:26:11 +0530

    Update sieve.cpp

--- sieve.cpp
+++ sieve.cpp
@@ -20,3 +20,9 @@ void sieve(int n) {
         }
     }
 }
+
+std::vector<std::vector<int>> divs(m + 1);
+for (int i = 1; i <= m; i++)
+    for (int j = i; j <= m; j += i)
+        divs[j].push_back(i);
+// divs[x] stores all divisors of x. Used to iterate over common divisors of pairs.

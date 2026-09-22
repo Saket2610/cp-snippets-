320bd415ea67ea9165346e18e6bf102100bc2910
Saket shaurya <69414031+Saket2610@users.noreply.github.com>
2026-09-22 11:00:01 +0530

    Update kmp.cpp

--- kmp.cpp
+++ kmp.cpp
@@ -1,6 +1,13 @@
 // Computes the prefix function pi (or failure function) for string s.
 // pi[i] is the length of the longest proper prefix of s[0...i] 
 // that is also a suffix of s[0...i].
+
+// p[0] = 0 
+// s = "abacaba"
+// i = 5 ("abacab"):
+// Substring ends with "ab". Matches prefix "ab" (length 2) -> p[5]=2
+// p = {0 , 0 , 1 , 0 , 1 , 2 , 3}
+// z = {7 , 0 , 1 , 0 , 3 , 0 , 1}
 std::vector<int> computePrefixFunction(const std::string& s) {
     int n = s.size();
     std::vector<int> pi(n, 0);
